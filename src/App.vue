5c1da81369ec7fbd0a67a9b04e20cba069471597
yinxiefei <yaya32524>
2019-06-14 20:26:38 +0800

    更新基本代码

--- src/App.vue
+++ src/App.vue
@@ -7,25 +7,26 @@
     <router-view/>
   </div>
 </template>
+<script>
+  import { mapGetters } from 'vuex'
+  export default{
+    computed: {
+      ...mapGetters(['text'])
+    },
+    mounted(){
+      this.$store.dispatch('setText', 11).then(()=>{
+        console.log(this.text)
+      })
+    } 
+  }
+  document.addEventListener('DOMContentLoaded', ()=>{
+    const html = document.querySelector('html');
+    let fontSize = window.innerWidth/10;
+    fontSize = fontSize > 50 ? 50 : fontSize;
+    html.style.fontSize = fontSize + 'px';
+  })
+</script>
+<style lang='sass'>
+    @import './assets/styles/global.scss'
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
